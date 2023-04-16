b50cefd15cbf70598e3de2430ce6b508e88584f6
Edwin Guerrero <edwinguerrerotech@gmail.com>
2023-04-16 11:47:47 -0400

    4th attempt.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,26 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  // ... other Webpack configurations ...
-
+  entry: 'assets/js/main.js', // Your entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output path
+    filename: 'bundle.js' // Output filename
+  },
+  module: {
+    rules: [
+      // Example rule for JavaScript transpilation using Babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     // Define the process.env object with the GITHUB_TOKEN value
     new webpack.DefinePlugin({
